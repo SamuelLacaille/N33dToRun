b452795b5eff170aed99a94a5674b9b51145ab97
SamuelLacaille <samuel76560@outlook.fr>
2021-06-15 14:48:20 +0200

    redirection vers  page  login si user non connecté

--- N33dToRun/src/app/app.component.ts
+++ N33dToRun/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -14,5 +19,29 @@ export class AppComponent {
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(
+    private router: Router,
+    public afAuth: AngularFireAuth,
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.afAuth.authState.subscribe(auth => {
+        if (!auth) {
+          console.log('non connecté');
+          this.router.navigateByUrl('/login');
+        } else {
+          this.router.navigateByUrl('/');
+          console.log('Connecté: ' + auth.uid);
+        }
+      });
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
 }
--- N33dToRun/src/app/login/login.module.ts
+++ N33dToRun/src/app/login/login.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { IonicModule, ToastController } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
@@ -17,4 +17,46 @@ import { LoginPage } from './login.page';
   ],
   declarations: [LoginPage]
 })
-export class LoginPageModule {}
+export class LoginPageModule {
+  loginData = {
+  email: '',
+  password: ''
+};
+constructor(
+  public toastController: ToastController,
+  public afAuth: AngularFireAuth
+) {  }
+
+login() {
+  this.afAuth.signInWithEmailAndPassword(this.loginData.email, this.loginData.password)
+  .then(auth => {
+    console.log('utilisateur connecté');
+  })
+  .catch(err => {
+    console.log('Erreur: ' + err);
+    this.errorMail();
+  });
+
+  
+}
+signUp() {
+  this.afAuth.createUserWithEmailAndPassword(this.loginData.email, this.loginData.password)
+  .then(auth => {
+    console.log('utilisateur connecté');
+  })
+  .catch(err => {
+    console.log('Erreur: ' + err);
+    this.errorMail();
+  });
+}
+
+async errorMail() {
+  const toast = await this.toastController.create({
+    message: 'Email ou mot de passe incorrect',
+    duration: 2000,
+    position: 'top'
+  });
+  toast.present();
+}
+
+}
