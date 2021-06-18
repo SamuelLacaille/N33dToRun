86579a87abb57abfc68f93a3d2f40459cf74b573
SamuelLacaille <samuel76560@outlook.fr>
2021-06-18 12:33:08 +0200

    back connexion ok

--- N33dToRun/src/app/inscription/inscription.page.ts
+++ N33dToRun/src/app/inscription/inscription.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../services/auth.service";
 
 @Component({
   selector: 'app-inscription',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionPage implements OnInit {
 
-  constructor() { }
+  constructor( public authService: AuthService) { }
 
   ngOnInit() {
   }
--- N33dToRun/src/app/login/login.page.ts
+++ N33dToRun/src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from "../services/auth.service";
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(  public authService: AuthService) { }
 
   ngOnInit() {
   }
