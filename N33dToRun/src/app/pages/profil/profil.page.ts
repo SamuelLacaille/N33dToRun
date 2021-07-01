86dee318b9eda742d373b93e9c5d20f9ff6bc360
SamuelLacaille <samuel76560@outlook.fr>
2021-07-01 16:03:24 +0200

    ajout notif 5 sec après acoir été sur l'accueil

--- N33dToRun/src/app/pages/profil/profil.page.ts
+++ N33dToRun/src/app/pages/profil/profil.page.ts
@@ -2,23 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 // import { PlaceService } from 'src/app/services/place.service';
 
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.page.html',
   styleUrls: ['./profil.page.scss'],
 })
-export class ProfilPage implements OnInit {
-    private loaded: boolean = false;
-    private places: Object;
-
-    constructor(){}
-
-    ngOnInit(){
-        this.load();
-    }
-
-    load(){
-        this.loaded = false;
+export class ProfilPage {
+    clickSub: any;
+    constructor(private localNotifications: LocalNotifications) {}
+    simpleNotif() {
+      this.localNotifications.schedule({
+        id: 1,
+        text: 'Vas faire du sport bg',
+        trigger: { at: new Date(new Date().getTime() + 5000) },
+        data: { secret: 'secret' }
+      });
     }
 }
